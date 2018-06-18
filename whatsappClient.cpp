da2378a2daf2b81ae2cd49ee18872d93989b0ab3
danielbraun89 <braundaniel@live.com>
2018-06-18 19:48:40 +0300

    nothing works

--- whatsappClient.cpp
+++ whatsappClient.cpp
@@ -1,7 +1,7 @@
 //
 // Created by danielbraun on 6/18/18.
 //
-
+#include "whatsappio.h"
 #include <iostream>
 #include <netdb.h>
 #include <unistd.h>
@@ -13,24 +13,82 @@
 #include <cerrno>
 #include <clocale>
 #include <cstring>
+#define MAXHOSTNAME 30
+
+char message_buffer[WA_MAX_MESSAGE];
+
+int read_data(int s, char *buf, int n) {
+    int bcount;
+/* counts bytes read */
+    int br;
+/* bytes read this pass */
+    bcount= 0; br= 0;
+    while (bcount < n) { /* loop until full buffer */
+        br = (int)read(s, buf, n-bcount);
+        if (br > 0) {
+            bcount += br;
+            if (buf[strlen(buf) - 1] == '\n')
+            {
+                buf += br;
+                return(bcount);
+            }
+            buf += br;
+        }
+        if (br < 1) {
+            return(-1);
+        }
+    }
+    return(bcount);
+}
+
+
+int write_data(int s,const char *buf, int n)
+
+{ int bcount,          /* counts bytes read */
+        br;              /* bytes read this pass */
+
+    bcount= 0;
+    br= 0;
+    while (bcount < n) {             /* loop until full buffer */
+        if ((br= write(s,buf,n-bcount)) > 0)
+        {
+            std::cout << "write_data!\n";
+            bcount += br;                /* increment byte counter */
+            buf += br;                   /* move buffer ptr for next read */
+        }
+        if (br < 0) {
+            perror("write_data");        /* signal an error to the caller */
+            return(-1);
+        }
+    }
+    return(bcount);
+}
+
+
 
 int call_socket(char *hostname, unsigned short portnum) {
-    struct sockaddr_in server_socket_address;
+    struct sockaddr_in server_socket_address_object;
     struct hostent *hp;
     int client_socket_file_descriptor;
-    if ((hp= gethostbyname (hostname)) == NULL) {
+
+    if ((hp= gethostbyname (hostname)) == nullptr)
+    {
         return(-1);
     }
-    memset(&server_socket_address,0,sizeof(server_socket_address));
-    memcpy((char *)&server_socket_address.sin_addr , hp->h_addr , (size_t)hp->h_length);
-    server_socket_address.sin_family = (sa_family_t)hp->h_addrtype;
-    server_socket_address.sin_port = htons((u_short)portnum);
+
+    memset(&server_socket_address_object,0,sizeof(struct sockaddr_in));
+    server_socket_address_object.sin_family = (sa_family_t)hp->h_addrtype;
+    memcpy(&server_socket_address_object.sin_addr , hp->h_addr , (size_t)hp->h_length);
+    server_socket_address_object.sin_port = htons((u_short)portnum);
+
+    std::cout << "server_socket_address_object initialized, with address: "<< inet_ntoa(server_socket_address_object.sin_addr)<< " port: "<< ntohs(server_socket_address_object.sin_port)  << std::endl;
 
     if ((client_socket_file_descriptor = socket(hp->h_addrtype, SOCK_STREAM, 0)) < 0) {
         return(-1);
     }
 
-    if (connect(client_socket_file_descriptor, (struct sockaddr *)&server_socket_address , sizeof(server_socket_address)) < 0) {
+    if (connect(client_socket_file_descriptor, (struct sockaddr *)&server_socket_address_object , sizeof(server_socket_address_object)) < 0)
+    {
         close(client_socket_file_descriptor);
         std::cout << std::strerror(errno) <<std::endl;
         return(-1);
@@ -43,14 +101,14 @@ int main(int argc, char *argv[])
 {
     if ((argc != 4) )
     {
-        std::cout << "Usage: whatsappClient clientName serverAddress serverPort" << std::endl;
+        print_client_usage();
         return -1;
     }
 
     char* p;
     long converted = strtol(argv[argc-1], &p, 10);
     if (*p) {
-        std::cout << "Usage: whatsappClient clientName serverAddress serverPort" << std::endl;
+        print_client_usage();
         return -1;
     }
 
@@ -64,5 +122,20 @@ int main(int argc, char *argv[])
     printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
 
     int client_socket_file_descriptor = call_socket(h->h_name, (unsigned short)converted);
+
+    std::string client_name (argv[1]);
+//    send(client_socket_file_descriptor, (const void *)(&client_name), client_name.length() );
+    while(1){
+        getchar();
+        write_data(client_socket_file_descriptor, client_name.c_str(), client_name.length());
+        write_data(client_socket_file_descriptor, "\n", 1);
+        printf("written client name");
+        std::cout << argv[1];
+        getchar();
+        read_data(client_socket_file_descriptor, message_buffer, WA_MAX_MESSAGE-1);
+        message_buffer[WA_MAX_MESSAGE-1] = 0;
+        std::cout << "got response from server: " << message_buffer;
+    }
+
     return 0;
 }
--- whatsappServer.cpp
+++ whatsappServer.cpp
@@ -1,9 +1,10 @@
 //
 // Created by danielbraun on 6/18/18.
 //
+
+#include "whatsappio.h"
 #define MAXHOSTNAME 30
 #define MAX_CLIENTS 200
-#define MAX_MESSAGE_LENGTH 256
 #define MAX_PENDING_CONNECTIONS 10
 #include <iostream>
 #include <netdb.h>
@@ -16,6 +17,40 @@
 #include <cerrno>
 #include <clocale>
 #include <cstring>
+#include <vector>
+
+int write_data(int s,const char *buf, int n)
+
+{ int bcount,          /* counts bytes read */
+            br;              /* bytes read this pass */
+
+    bcount= 0;
+    br= 0;
+    while (bcount < n) {             /* loop until full buffer */
+        if ((br= write(s,buf,n-bcount)) > 0)
+        {
+            std::cout << "write_data!\n";
+            bcount += br;                /* increment byte counter */
+            buf += br;                   /* move buffer ptr for next read */
+        }
+        if (br < 0) {
+            perror("write_data");        /* signal an error to the caller */
+            return(-1);
+        }
+    }
+    return(bcount);
+}
+
+struct client {
+    int fid;
+    std::string str_name;
+    char name[WA_MAX_NAME];
+};
+char message_buffer[WA_MAX_MESSAGE];
+
+fd_set clientsfds;
+fd_set readfds;
+std::vector<client> client_vector;
 
 int read_data(int s, char *buf, int n) {
     int bcount;
@@ -42,51 +77,89 @@ int read_data(int s, char *buf, int n) {
 }
 
 
+int get_connection(int server_socket_file_descriptor) {
+    int t; /* socket of connection */
+    if ((t = accept(server_socket_file_descriptor,NULL,NULL)) < 0)
+        exit(1);
+    return t;
+}
 
-void terminateServer(){};
-void connectNewClient(){
+void terminateServer()
+{
+    exit(0);
+};
+void connectNewClient(int server_socket_file_descriptor){
     std::cout << "a client want to connect! " << std::endl;
+    int new_client_socket_file_descriptor = get_connection(server_socket_file_descriptor);
+    std::cout << "a new socket has been created, num: " << new_client_socket_file_descriptor << std::endl;
 
-//    int get_connection(int s) {
-//        int t; /* socket of connection */
-//        if ((t = accept(s,NULL,NULL)) < 0)
-//            return -1;
-//        return t;
-//    }
+    struct client cur_client;
+    cur_client.fid = new_client_socket_file_descriptor;
+    client_vector.push_back(cur_client);
+
+    read_data(cur_client.fid, message_buffer, WA_MAX_MESSAGE-1);
+    message_buffer[WA_MAX_MESSAGE-1] = 0;
+    std::string str1 (message_buffer);
+    std::cout << "fresh client read: " << str1 <<std::endl;
+    FD_SET(cur_client.fid, &clientsfds);
 };
 
 void serverStdInput(){
-    char buf[MAX_MESSAGE_LENGTH];
-    read_data(STDIN_FILENO, buf, MAX_MESSAGE_LENGTH-1);
-    buf[MAX_MESSAGE_LENGTH] = 0;
-    std::cout << "read from stdin: " << buf << " with len: " << strlen(buf) << std::endl;
+    read_data(STDIN_FILENO, message_buffer, WA_MAX_MESSAGE-1);
+    message_buffer[WA_MAX_MESSAGE-1] = 0;
+    std::string str1 (message_buffer);
+//    std::cout << "read from stdin: " << message_buffer << " with len: " << str1.length() << std::endl;
+    std::string str2 ("EXIT\n");
+
+    if ((str1.compare(str2)) == 0)
+    {
+        terminateServer();
+    }
 
 };
 
 
-void handleClientRequest(){};
+void handleClientRequest(){
+    std::vector<struct client> client_vector2 = client_vector;
+    for (int i=0; i<client_vector.size(); i++)
+    {
+        if (FD_ISSET(client_vector[i].fid, &readfds))
+        {
+            int fid = client_vector[i].fid;
+            read_data(fid, message_buffer, WA_MAX_MESSAGE-1);
+            message_buffer[WA_MAX_MESSAGE-1] = 0;
+            std::string str1 (message_buffer);
+            std::cout << "request from client: " << str1 << " with len: " << str1.length() << " client num: " <<client_vector[i].fid << std::endl;
+            FD_SET(client_vector[i].fid, &clientsfds);
+            std::string str2 ("got it");
+            write_data(client_vector[i].fid, str2.c_str(), str2.length());
+
+        }
+    }
+};
 int main(int argc, char *argv[])
 {
     //input validation
     if ((argc != 2) )
     {
-        std::cout << "Usage: whatsappServer portNum" << std::endl;
-        return -1;
+        print_server_usage();
+        exit(1);
     }
     char* p;
     long converted = strtol(argv[argc-1], &p, 10);
     if (*p) {
-        std::cout << "Usage: whatsappServer portNum" << std::endl;
-        return -1;
+        print_server_usage();
+        exit(1);
     }
 
     uint16_t portnum = (uint16_t)converted;
-    char myname[MAXHOSTNAME+1];
+
     int server_socket_file_descriptor;
     struct sockaddr_in server_socket_address_object;
     struct hostent* hp;
 
     //hostnet initialization
+    char myname[MAXHOSTNAME+1];
     gethostname(myname, MAXHOSTNAME);
     hp = gethostbyname(myname);  //TODO CHECK RETURN VALUE
     if (hp == nullptr)
@@ -97,12 +170,13 @@ int main(int argc, char *argv[])
     server_socket_address_object.sin_family = (sa_family_t)hp->h_addrtype;
     memcpy(&server_socket_address_object.sin_addr, hp->h_addr, (size_t)hp->h_length);
     server_socket_address_object.sin_port= htons(portnum);
+    server_socket_address_object.sin_addr.s_addr = INADDR_ANY;
 
     std::cout << "server_socket_address_object initialized, with address: "<< inet_ntoa(server_socket_address_object.sin_addr)<< " port: "<< ntohs(server_socket_address_object.sin_port)  << std::endl;
 
     /* create socket */
     if ((server_socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        return(-1);
+        exit(1);
 
     if (bind(server_socket_file_descriptor , (struct sockaddr *)&server_socket_address_object , sizeof(struct
             sockaddr_in)) < 0)
@@ -114,27 +188,28 @@ int main(int argc, char *argv[])
 
     listen(server_socket_file_descriptor, MAX_PENDING_CONNECTIONS);
 
-    fd_set clientsfds;
-    fd_set readfds;
 
     FD_ZERO(&clientsfds);
     FD_SET(server_socket_file_descriptor, &clientsfds);
     FD_SET(STDIN_FILENO, &clientsfds);
     int stillRunning = 1;
-    while (stillRunning) {
+    while (stillRunning)
+    {
         readfds = clientsfds;
-        if (select(MAX_CLIENTS+1, &readfds, NULL,
-                   NULL, NULL) < 0) {
+        if (select(MAX_CLIENTS+1, &readfds, NULL,NULL, NULL) < 0)
+        {
             terminateServer();
             return -1;
         }
 
-        if (FD_ISSET(server_socket_file_descriptor, &readfds)) {
+        else if (FD_ISSET(server_socket_file_descriptor, &readfds))
+        {
             //will also add the client to the clientsfds
-            connectNewClient();
+            connectNewClient(server_socket_file_descriptor);
         }
 
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+        else if (FD_ISSET(STDIN_FILENO, &readfds))
+        {
             serverStdInput();
         }
 
